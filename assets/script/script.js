5a661fc85ca7525be505a351f2c729af9201bf33
Tien Sy <tiensy002@gmail.com>
2023-02-09 14:14:24 +0700

    fontend-done

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+                //SLIDER
+var swiper = new Swiper(".slider-wrapper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+                //Customer's Review
+var swiper = new Swiper(".feedback-wrap", {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
